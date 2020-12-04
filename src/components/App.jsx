11adbbb11f0bfee002570799e9b4e9a1e1489b5f
Kimberly Gehrmann <gehrmann.kimberly@gmail.com>
2020-12-03 18:07:57 -0600

    forgot to save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './videoPlayer.js';
-// import Search from './search.js';
+import Search from './search.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
@@ -91,20 +91,22 @@ class App extends React.Component {
     return (<div>
       <nav className="navbar">
         <div className="col-md-6 offset-md-3">
-          <div><h5><em>search</em> view goes here</h5></div>
+          <Search userTyped={this.search.bind(this)}/>
         </div>
       </nav>
       <div className="row">
         <div className="col-md-7">
-          {/* <div><h5><em>videoPlayer</em> view goes here</h5></div> */}
           <VideoPlayer video={this.state.currentVideo} />
         </div>
         <div className="col-md-5">
-          {/* <div><h5><em>videoList</em> view goes here</h5></div> */}
           <VideoList videos={this.state.videoList} handleTitleClick={this.handleTitleClick.bind(this)} />
         </div>
       </div>
     </div>);
+  }
+
+  search() {
+
   }
 
   componentDidMount() {
